1cb87b438651d173d7282af308507775fd92ad4f
gatk6100 <143455265+gatk6100@users.noreply.github.com>
2023-10-01 19:27:18 +0000

    Starting the practice problem

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Write a Snippet of code that will ask for a student's exam score
+// then print "Good Job" if the score is over 80;
+// otherwise, print "Please Work A Little Harder".
+// Continue asking for exam scores and printing the appropriate
+// message until the user enters a negative score
+
+#include <namespace>
+#include <iomanip>
+#include <cmath>
+#include <string>
